e4cdc14ac0856a0a9d02106e4e5e803342ad45ca
Cara Weggler <gx3009iy@t3040-1-6.local>
2019-01-15 19:12:17 -0600

    Hello world message app first commit

--- hello world/ViewController.swift
+++ hello world/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showMessage(_ sender: Any) {
+        helloMessage.text = "Hello, iOS Programmer!"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
